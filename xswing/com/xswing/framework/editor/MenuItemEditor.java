5be73578c144b8f554d4efa11448b83b4a10aded
havyan <havyan@gmail.com>
2015-11-30 16:05:37 +0800

    Pending

---
+++ xswing/com/xswing/framework/editor/MenuItemEditor.java
@@ -0,0 +1,34 @@
+package com.xswing.framework.editor;
+
+import javax.swing.JMenuItem;
+
+import com.xswing.framework.action.Action;
+
+@Component(types = { JMenuItem.class })
+public class MenuItemEditor extends AbstractEditor<JMenuItem, String> {
+
+	@Override
+	public void setValue(Object value) {
+		component.setText((String) value);
+	}
+
+	@Override
+	public String getValue() {
+		return component.getText();
+	}
+
+	@Override
+	public void setEnabled(boolean enabled) {
+		component.setEnabled(enabled);
+	}
+
+	@Override
+	public void reset() {
+		component.setText("");
+	}
+
+	public void registerAction(Action<?, ?, ?> action) {
+		component.addActionListener(action);
+	}
+
+}
