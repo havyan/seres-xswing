ec8ec413468bc8c03db4135c60047922c9faa1ec
havyan <havyan@gmail.com>
2017-02-23 00:04:58 -0600

    Pending

--- xswing/com/xswing/framework/view/parser/ImportParser.java
+++ xswing/com/xswing/framework/view/parser/ImportParser.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.jdom2.Element;
 
 import com.xswing.framework.model.AppModel;
+import com.xswing.framework.model.DefaultAppModel;
 import com.xswing.framework.view.Context;
 import com.xswing.framework.view.XPanel;
 import com.xswing.framework.view.XPanelBuilder;
@@ -24,6 +25,10 @@ public class ImportParser extends ComponentParser<XPanel> {
 	@Override
 	public XPanel parseElement(Context context, Element source) {
 		AppModel<?> model = context.getModel();
+		String value = getString(source, Const.VALUE);
+		if (StringUtils.isNoneEmpty(value)) {
+			model = new DefaultAppModel<Object>();
+		}
 		String modelRef = getString(source, Const.MODEL);
 		if (StringUtils.isNotEmpty(modelRef)) {
 			Object bean = context.getBean(modelRef);
